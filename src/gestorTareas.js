743864277b3e2f7ae41f93d46c270f32ec438b1d
DiegoDelgadillo <ddcabrera69@gmail.com>
2021-11-10 14:59:37 -0400

    clase añadida

--- src/gestorTareas.js
+++ src/gestorTareas.js
@@ -22,22 +22,32 @@
 //   lista_elem.innerHTML = tarea_input.value;
 // });
 
-
+let tarea = class Tarea {
+  constructor(id,titulo, descripcion, fechaLim, categoria) {
+    this.id = id;
+    this.titulo = titulo;
+    this.descripcion = descripcion;
+    this.fechaLim = fechaLim;
+    this.categoria = categoria;
+  }
+}
 
 
 const tarea_elem = document.querySelector("#tarea");
- const boton_elem = document.querySelector("#crear-tarea");
- const lista_elem = document.querySelector("#lista-tareas");
+const boton_elem = document.querySelector("#crear-tarea");
+const lista_elem = document.querySelector("#lista-tareas");
 
- boton_elem.addEventListener("click", (event) => {
-   lista_elem.innerHTML = tarea_elem.value;
- });
+boton_elem.addEventListener("click", (event) => {
+  tarea.titulo = tarea_elem.value;
+  lista_elem.innerHTML = tarea.titulo;
+});
 
- //solo como ejemplo para mostrar como funciona con un form
- const tarea_input = document.querySelector("#tarea-input");
- const form_elem = document.querySelector("#form-crear");
+//solo como ejemplo para mostrar como funciona con un form
+const tarea_input = document.querySelector("#tarea-input");
+const form_elem = document.querySelector("#form-crear");
 
- form_elem.addEventListener("submit", (event) => {
-   event.preventDefault();
-   lista_elem.innerHTML = tarea_input.value;
- });
+form_elem.addEventListener("submit", (event) => {
+  event.preventDefault();
+  tarea.titulo = tarea_input.value;
+  lista_elem.innerHTML = tarea.titulo;
+});
